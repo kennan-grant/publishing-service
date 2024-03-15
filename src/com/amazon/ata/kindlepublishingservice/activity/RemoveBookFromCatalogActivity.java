fe778ae1799c4ed9618790c798946ad25e684368
Kennan Grant <kennangrant@gmail.com>
2024-03-15 11:39:19 -0400

    tests passing

--- src/com/amazon/ata/kindlepublishingservice/activity/RemoveBookFromCatalogActivity.java
+++ src/com/amazon/ata/kindlepublishingservice/activity/RemoveBookFromCatalogActivity.java
@@ -1,15 +1,29 @@
 package com.amazon.ata.kindlepublishingservice.activity;
 
+import com.amazon.ata.kindlepublishingservice.converters.CatalogItemConverter;
+import com.amazon.ata.kindlepublishingservice.converters.RecommendationsCoralConverter;
+import com.amazon.ata.kindlepublishingservice.dynamodb.models.CatalogItemVersion;
 import com.amazon.ata.kindlepublishingservice.models.requests.RemoveBookFromCatalogRequest;
+import com.amazon.ata.kindlepublishingservice.models.response.GetBookResponse;
 import com.amazon.ata.kindlepublishingservice.models.response.RemoveBookFromCatalogResponse;
+import com.amazon.ata.recommendationsservice.types.BookGenre;
+import com.amazon.ata.recommendationsservice.types.BookRecommendation;
 import com.amazonaws.services.lambda.runtime.Context;
 
 import javax.inject.Inject;
+import java.util.List;
 
 public class RemoveBookFromCatalogActivity {
     @Inject
     RemoveBookFromCatalogActivity() {}
     public RemoveBookFromCatalogResponse execute(RemoveBookFromCatalogRequest removeBookFromCatalogRequest) {
-        return null;
+        CatalogItemVersion catalogItem = catalogDao.getBookFromCatalog(request.getBookId());
+        List<BookRecommendation> recommendations = recommendationServiceClient.getBookRecommendations(
+                BookGenre.valueOf(catalogItem.getGenre().name()));
+
+        return RemoveBookFromCatalogResponse.builder()
+                .withBook(CatalogItemConverter.toBook(catalogItem))
+                .withRecommendations(RecommendationsCoralConverter.toCoral(recommendations))
+                .build();
     }
 }
